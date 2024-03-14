58e5db4357e71d8693543ef3ea5a17dfbdd4e5ed
Carlos Chile <150838094+CarlosJChileS@users.noreply.github.com>
2024-03-14 15:06:36 -0500

    1

--- Introduccion.py
+++ Introduccion.py
@@ -87,4 +87,14 @@ resultado_numeros= numero_uno > numero_dos
 print(resultado_numeros)
 
 #PART 4
-#
+#PALABRAS RESERVADAS
+"""Aquí el listado de todas ellas:
+
+ 	 	 	 
+False	class	is	return
+None	continue	lambda	try
+True	def	nonlocal	while
+and	del	not	with
+as	elif	or	yield
+assert	else	pass	*
+break	except	raise	*"""
