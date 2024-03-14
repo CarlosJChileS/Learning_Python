e5ffaf5d346ba1410e9221726bb2cd2b1fd7255f
Carlos Chile <150838094+CarlosJChileS@users.noreply.github.com>
2024-03-14 15:57:10 -0500

    5

--- Diccionario.py
+++ Diccionario.py
@@ -30,3 +30,48 @@ valor =diccionario.setdefault('e',5)
 
 print(valor)
 print(diccionario)
+
+#PART 2
+#LLAVES, ITEMS Y VALORES
+diccionario = {'a':1, 'b': 2,'c':3, 'd':4}
+
+#keys
+#values
+# items
+
+#retornara lo que se encuentra almacenado en las llavees
+llaves = tuple(diccionario.keys())
+print(llaves)
+
+#obterner valores
+valores = diccionario.values()
+print(valores)
+
+elementos= tuple(diccionario.items())
+print(elementos)
+#formas de conocer los elementos de un diccionario
+
+#PART 3
+#ELIMINAR ELEMENTOS DEL DICCIONARIO
+diccionario = {'a':1, 'b': 2,'c':3, 'd':4}
+
+
+#1era forma es del
+#pop 
+
+#longitud del diccionario
+print(len(diccionario))
+
+#si no existe la llave saldra error
+del diccionario['a'] #1 
+#del se usa para eliminar
+
+valor = diccionario.pop('b') #eliminar 2
+
+#clear para eliminar todos los elementos del diccionario
+#diccionario.clear()
+
+
+print(diccionario)
+print(len(diccionario))
+
